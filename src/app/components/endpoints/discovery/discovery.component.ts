c6bfc0b89b675774d09a65a8fa6201ffae0b5853
Karol Błażejczyk <blazkaro.programmer@protonmail.com>
2023-07-13 14:32:25 +0200

    short doc for discovery endpoint

---
+++ src/app/components/endpoints/discovery/discovery.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-discovery',
+  templateUrl: './discovery.component.html',
+  styleUrls: ['./discovery.component.css']
+})
+export class DiscoveryComponent {
+
+}
